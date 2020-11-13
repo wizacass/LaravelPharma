b475385aed29cf336d32d85ff38af709cd02d29c
Vytenis Petrauskas <visak.pet0@gmail.com>
2020-11-13 20:19:33 +0200

    Update index page with auth buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,7 @@
         </div>
     </section>
 
+    @auth
     <div class="tile is-ancestor has-text-centered" style="margin:1em">
         <div class="tile is-vertical is-parent">
             <div class="tile is-child">
@@ -40,4 +41,25 @@
             </div>
         </div>
     </div>
+    <div class="has-text-centered">
+        <a class="button is-success is-outlined is-large">Logout</a>
+    </div>
+    @else
+    <div class="tile is-ancestor has-text-centered" style="margin:1em">
+        <div class="tile is-vertical is-parent">
+            <div class="tile is-child">
+                <article class="tile is-child notification is-success">
+                    <a class="title" href="/register">Signup</a>
+                </article>
+            </div>
+        </div>
+        <div class="tile is-vertical is-parent">
+            <div class="tile is-child">
+                <article class="tile is-child notification is-success">
+                    <a class="title" href="/login">Login</a>
+                </article>
+            </div>
+        </div>
+    </div>
+    @endauth
 @endsection
